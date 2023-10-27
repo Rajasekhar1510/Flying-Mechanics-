3f72df072b328b75b90933a03cc71ff7e1450e09
pvs20csu283 <101687260+pvs20csu283@users.noreply.github.com>
2023-10-28 00:02:10 +0530

    procedural terrain 2 commit

--- Invincible/Assets/Scripts/MeshGenerator.cs
+++ Invincible/Assets/Scripts/MeshGenerator.cs
@@ -3,77 +3,70 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-[RequireComponent(typeof(MeshFilter))]  
+[RequireComponent(typeof(MeshFilter))]
 public class MeshGenerator : MonoBehaviour
 {
-    Mesh mesh;
-
+    [SerializeField] Mesh mesh;
+    [SerializeField] MeshFilter meshFilter;
+    [SerializeField] Vector2Int size;
     Vector3[] vertices;
     int[] triangles;
 
-    public int xSize = 20;
-    public int zSize = 20;
-
-
-    void Start()
+    int[] CreateTraingles()
     {
-        mesh = new Mesh();
-        GetComponent<MeshFilter>().mesh = mesh;
-
-        CreateShape();
         
-    }
-    private void Update()
-    {
-        UpdateMesh();
-    }
-    // Update is called once per frame
-    void CreateShape()
-    {
-        vertices = new Vector3[(xSize + 1) * (zSize + 1)];
-        
-        for(int i = 0, z  = 0; z <= zSize; z++) 
+        triangles = new int[size.x * size.y * 6];
+
+        for (int z = 0, vert = 0, tris = 0; z < size.y; z++)
         {
-            for (int x = 0; x <= xSize; x++)
-            {
-                vertices[i] = new Vector3(x, 0, z);
-                i++;
-            }
-        }
-        
-        triangles = new int[xSize * zSize* 6];
-
-        
-        
-
-        for (int z = 0, vert = 0, tris = 0; z < zSize; z++)
-        {
-            for (int x = 0; x <= xSize; x++)
+            for (int x = 0; x < size.x; x++)
             {
                 triangles[tris + 0] = vert + 0;
-                triangles[tris + 1] = vert + xSize + 1;
+                triangles[tris + 1] = vert + size.x + 1;
                 triangles[tris + 2] = vert + 1;
                 triangles[tris + 3] = vert + 1;
-                triangles[tris + 4] = vert + xSize + 1;
-                triangles[tris + 5] = vert + xSize + 1;
+                triangles[tris + 4] = vert + size.x + 1;
+                triangles[tris + 5] = vert + size.x + 2;
                 vert++;
                 tris += 6;
             }
             vert++;
         }
-        
-
+        return triangles;
     }
 
-    void UpdateMesh()
+    private Vector3[] CreateVertices()
     {
-        mesh.Clear();
+        vertices = new Vector3[(size.x + 1) * (size.y + 1)];
+
+        for (int i = 0, z = 0; z <= size.y; z++)
+        {
+            for (int x = 0; x <= size.x; x++)
+            {
+                vertices[i] = new Vector3(x, 0, z);
+                i++;
+            }
+        }
+
+        return vertices;
+    }
+
+    void Generate()
+    {
+        mesh = new Mesh();
+       // mesh.Clear();
 
         mesh.vertices = vertices;
         mesh.triangles = triangles;
 
         mesh.RecalculateNormals();
+        meshFilter.sharedMesh = mesh;
     }
 
-
+    private void Start()
+    {
+        Generate();
+        CreateTraingles();
+        CreateVertices();
+    }
 }
